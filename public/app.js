d417692546833a3f514c1f5339ddd0ca32c8088d
thedigitalgarage <johnmccawley@outlook.com>
2017-02-03 19:02:01 -0700

    add input forms

--- public/app.js
+++ public/app.js
@@ -10,5 +10,10 @@ app.controller('MainCtrl', [
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}
     ];
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
+    };
   }
 ]);
