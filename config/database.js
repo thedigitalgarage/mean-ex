e122c07520feee31242e68f2a6a12cc1340f73d9
thedigitalgarage <johnmccawley@outlook.com>
2017-02-11 08:48:18 -0700

    update database connection url

--- config/database.js
+++ config/database.js
@@ -1,14 +1,12 @@
 var mongoServiceName = process.env.DATABASE_SERVICE_NAME,
-    mongoHost = 'mongodb',
-    mongoPort = '27017',
     mongoDatabase = process.env.MONGODB_DATABASE,
     mongoPassword = process.env.MONGODB_PASSWORD,
     mongoUser = process.env.MONGODB_USER;
 
 if (process.env.DATABASE_SERVICE_NAME) {
-  var mongodburl = mongoHost + '://' + mongoUser + ':' + mongoPassword + '@' + mongoServiceName +':' + mongoPort + '/' + mongoDatabase;
+  var mongodburl = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoServiceName +':27017/' + mongoDatabase;
 } else {
-  var mongodburl = 'mongodb://testuser1:mountain1@127.0.0.1:27017/sampledb';
+  var mongodburl = 'mongodb://' + mongoUser + ':' + mongoPassword + '@127.0.0.1:27017/' + mongoDatabase;
 }
 
 console.log(mongodburl)
