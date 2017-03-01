17cc9e74b25b766100088be9a1f89e490f5fee04
thedigitalgarage <johnmccawley@outlook.com>
2017-03-01 15:15:40 -0700

    Update Express and AngularJS versions

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var fs = require('fs')
 var mongoose = require('mongoose'); // mongoose for mongodb
 var morgan = require('morgan'); // log requests to the console (express4)
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var database = require('./config/database'); //load the database config
 
 // configuration =================
@@ -16,7 +15,6 @@ app.use(morgan('combined')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride());
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
