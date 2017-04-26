33f4d8b02ed5b5cf14834f7cee673066162e17d1
John McCawley <john.mccawley@thedigitalgarage.io>
2017-04-26 14:22:30 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,9 +1,51 @@
-// public/core.js
-var app = angular.module('helloWorld', [])
+// public/app.js
+var helloTaskList = angular.module('helloTaskList', [])
 
-    .controller('MainCtrl', [
-        '$scope',
-        function($scope) {
-            $scope.test = 'Hello World! Your first MEAN Stack App on Digital Garage';
-        }
-    ]);
+    .controller('MainCtrl', function($scope, $http) {
+            $scope.formData = {};
+
+            // when landing on the page, get all tasks and show them
+            $http.get('/api/tasks')
+                .then(
+                    function(response) {
+                        // success callback
+                        $scope.tasks = response.data;
+                        console.log(response.data);
+                    },
+                    function(error) {
+                        // failure call back
+                        console.log('Error: ' + error);
+                    }
+                );
+
+            // when submitting the add form, send the text to the node API
+            $scope.createTask = function() {
+                $http.post('/api/tasks', $scope.formData)
+                    .then(
+                        function(response) {
+                            $scope.formData = {}; // clear the form so our user is ready to enter another
+                            $scope.tasks = response.data;
+                            console.log(response.data);
+                        },
+                        function(error) {
+                            console.log('Error: ' + error);
+                        }
+                    );
+            };
+
+            // delete a task after checking it
+            $scope.deleteTask = function(id) {
+                $http.delete('/api/tasks/' + id)
+                    .then(
+                        function(response) {
+                            // success callback
+                            $scope.tasks = response.data;
+                            console.log(response.data);
+                        },
+                        function(error) {
+                            // failure call back
+                            console.log('Error: ' + error);
+                        }
+                    );
+            };
+        });
